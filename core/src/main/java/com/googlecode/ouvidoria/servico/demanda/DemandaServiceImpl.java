4dcf72248ea1f55d1dfec47fcc0f44737c1bdde2
phtcosta <phtcosta@b14c6362-024a-0410-a50b-db045366b063>
2008-04-04 03:34:04 +0000

    criando o esqueleto de uma classe de servico. Para executar o primeiro teste com a pagina helloworld eh preciso ter uma classe com o esteriotipo service

---
+++ core/src/main/java/com/googlecode/ouvidoria/servico/demanda/DemandaServiceImpl.java
@@ -0,0 +1,15 @@
+// license-header java merge-point
+/**
+ * This is only generated once! It will never be overwritten.
+ * You can (and have to!) safely modify it by hand.
+ */
+package com.googlecode.ouvidoria.servico.demanda;
+
+/**
+ * @see com.googlecode.ouvidoria.servico.demanda.DemandaService
+ */
+public class DemandaServiceImpl
+    extends com.googlecode.ouvidoria.servico.demanda.DemandaServiceBase
+{
+
+}
