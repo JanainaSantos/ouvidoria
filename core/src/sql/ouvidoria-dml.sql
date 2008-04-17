4ed2075caa62ae099655412a5d8b9e20dbca223b
phtcosta <phtcosta@b14c6362-024a-0410-a50b-db045366b063>
2008-04-17 19:41:28 +0000

    populando tela de cadastro de demanda

---
+++ core/src/sql/ouvidoria-dml.sql
@@ -0,0 +1,28 @@
+insert into forma_contato(nome,descricao) values('E-mail','Contato feito atraves de email');
+insert into forma_contato(nome,descricao) values('Telefone','Contato feito atraves de telefone');
+insert into forma_contato(nome,descricao) values('Carta','Contato feito atraves de carta');
+insert into forma_contato(nome,descricao) values('Pessoalmente','Contato feito pessoalmente');
+insert into forma_contato(nome,descricao) values('Site','Contato feito atraves do site da ouvidoria');
+
+insert into tipo_demanda(nome,descricao) values('Critica','...');
+insert into tipo_demanda(nome,descricao) values('Denuncia','...');
+insert into tipo_demanda(nome,descricao) values('Elogio','...');
+insert into tipo_demanda(nome,descricao) values('Reclamacao','...');
+insert into tipo_demanda(nome,descricao) values('Sugestao','...');
+
+insert into assunto(nome,descricao) values('Agricultura Familiar','...');
+insert into assunto(nome,descricao) values('Estoques Publicos','...');
+insert into assunto(nome,descricao) values('Programas de Subvencao Economica','...');
+insert into assunto(nome,descricao) values('Doacao de Alimentos','...');
+insert into assunto(nome,descricao) values('Armazenagem','...');
+insert into assunto(nome,descricao) values('Convenios e Transferencias','...');
+insert into assunto(nome,descricao) values('PNUD','...');
+insert into assunto(nome,descricao) values('Levantamento de Safras','...');
+insert into assunto(nome,descricao) values('Recursos Financeiros/Orcamentarios/Contabil','...');
+insert into assunto(nome,descricao) values('Recursos Humanos','...');
+insert into assunto(nome,descricao) values('Recursos Materiais','...');
+insert into assunto(nome,descricao) values('Patrimonio','...');
+insert into assunto(nome,descricao) values('Tecnologia da Informacao','...');
+insert into assunto(nome,descricao) values('Licitacao e Contratos','...');
+insert into assunto(nome,descricao) values('Outros ...','...');
+
