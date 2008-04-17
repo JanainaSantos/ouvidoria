9ff7f7b018314569655442b341828133501f4139
phtcosta <phtcosta@b14c6362-024a-0410-a50b-db045366b063>
2008-04-17 13:15:45 +0000

    iniciando caso de uso AutenticaUsuario

---
+++ core/src/java/com/googlecode/ouvidoria/servico/autentica/UsuarioServiceImpl.java
@@ -0,0 +1,25 @@
+// license-header java merge-point
+/**
+ * This is only generated once! It will never be overwritten.
+ * You can (and have to!) safely modify it by hand.
+ */
+package com.googlecode.ouvidoria.servico.autentica;
+
+/**
+ * @see com.googlecode.ouvidoria.servico.autentica.UsuarioService
+ */
+public class UsuarioServiceImpl
+    extends com.googlecode.ouvidoria.servico.autentica.UsuarioServiceBase
+{
+
+    /**
+     * @see com.googlecode.ouvidoria.servico.autentica.UsuarioService#autenticarUsuario(java.lang.String, java.lang.String)
+     */
+    protected com.googlecode.ouvidoria.negocio.autentica.Usuario handleAutenticarUsuario(java.lang.String login, java.lang.String senha)
+        throws java.lang.Exception
+    {
+        //@todo implement protected com.googlecode.ouvidoria.negocio.autentica.Usuario handleAutenticarUsuario(java.lang.String login, java.lang.String senha)
+        return null;
+    }
+
+}
