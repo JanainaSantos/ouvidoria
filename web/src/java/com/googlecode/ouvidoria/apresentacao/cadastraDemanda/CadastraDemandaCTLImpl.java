624f5375a9eadda366ccc53852c5ac917052f77c
phtcosta <phtcosta@b14c6362-024a-0410-a50b-db045366b063>
2008-04-17 21:01:37 +0000

    dividindo o caso de uso cadastraDemanda em dois cenarios (cadastra demanda e cadastra demandante)

--- web/src/java/com/googlecode/ouvidoria/apresentacao/cadastraDemanda/CadastraDemandaCTLImpl.java
+++ web/src/java/com/googlecode/ouvidoria/apresentacao/cadastraDemanda/CadastraDemandaCTLImpl.java
@@ -12,13 +12,29 @@ import org.apache.struts.action.ActionMapping;
 import com.googlecode.ouvidoria.negocio.demanda.Assunto;
 import com.googlecode.ouvidoria.negocio.demanda.FormaContato;
 import com.googlecode.ouvidoria.negocio.demanda.TipoDemanda;
+import com.googlecode.ouvidoria.negocio.demandante.Estado;
 
 /**
  * @see com.googlecode.ouvidoria.apresentacao.cadastraDemanda.CadastraDemandaCTL
  */
 public class CadastraDemandaCTLImpl extends CadastraDemandaCTL
 {
-    /**
+	  /**
+     * @see com.googlecode.ouvidoria.apresentacao.cadastraDemanda.CadastraDemandaCTL#cadastraDemanda(org.apache.struts.action.ActionMapping, com.googlecode.ouvidoria.apresentacao.cadastraDemanda.CadastraDemandaForm, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+     */
+    public final void cadastraDemanda(ActionMapping mapping, com.googlecode.ouvidoria.apresentacao.cadastraDemanda.CadastraDemandaForm form, HttpServletRequest request, HttpServletResponse response) throws Exception
+    {
+    	//TODO 
+        
+    }
+
+	@Override
+	public void preCadastroDemanda(ActionMapping mapping, PreCadastroDemandaForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
      * @see com.googlecode.ouvidoria.apresentacao.cadastraDemanda.CadastraDemandaCTL#recuperaFormasContato(org.apache.struts.action.ActionMapping, com.googlecode.ouvidoria.apresentacao.cadastraDemanda.RecuperaFormasContatoForm, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
      */
     public final void recuperaFormasContato(ActionMapping mapping, com.googlecode.ouvidoria.apresentacao.cadastraDemanda.RecuperaFormasContatoForm form, HttpServletRequest request, HttpServletResponse response) throws Exception
@@ -29,8 +45,6 @@ public class CadastraDemandaCTLImpl extends CadastraDemandaCTL
     		FormaContato forma = (FormaContato) it.next();
     		mapa.put(forma.getId(), forma.getNome());
     	}
-    	
-        // this property receives a default value, just to have the application running on dummy data
         //form.setFormaContato(new Long((long)-2113222835));
         form.setFormaContatoValueList(mapa.keySet().toArray());
         form.setFormaContatoLabelList(mapa.values().toArray());
@@ -47,8 +61,6 @@ public class CadastraDemandaCTLImpl extends CadastraDemandaCTL
     		TipoDemanda tipo = (TipoDemanda) it.next();
     		mapa.put(tipo.getId(), tipo.getNome());
     	}
-    	
-        // this property receives a default value, just to have the application running on dummy data
         //form.setFormaContato(new Long((long)-2113222835));
         form.setTipoDemandaValueList(mapa.keySet().toArray());
         form.setTipoDemandaLabelList(mapa.values().toArray());        
@@ -65,33 +77,36 @@ public class CadastraDemandaCTLImpl extends CadastraDemandaCTL
     		Assunto assunto = (Assunto) it.next();
     		mapa.put(assunto.getId(), assunto.getNome());
     	}
-    	
-        // this property receives a default value, just to have the application running on dummy data
         //form.setFormaContato(new Long((long)-2113222835));
         form.setAssuntoValueList(mapa.keySet().toArray());
         form.setAssuntoLabelList(mapa.values().toArray());  
     }
 
-    /**
-     * @see com.googlecode.ouvidoria.apresentacao.cadastraDemanda.CadastraDemandaCTL#cadastraDemanda(org.apache.struts.action.ActionMapping, com.googlecode.ouvidoria.apresentacao.cadastraDemanda.CadastraDemandaForm, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
-     */
-    public final void cadastraDemanda(ActionMapping mapping, com.googlecode.ouvidoria.apresentacao.cadastraDemanda.CadastraDemandaForm form, HttpServletRequest request, HttpServletResponse response) throws Exception
-    {
-    	//TODO 
-        // this property receives a default value, just to have the application running on dummy data
-        form.setMensagem("mensagem-test");
-        // this property receives a default value, just to have the application running on dummy data
-        form.setAssunto(new Long((long)-704303915));
-        form.setAssuntoValueList(new Object[] {"assunto-1", "assunto-2", "assunto-3", "assunto-4", "assunto-5"});
-        form.setAssuntoLabelList(form.getAssuntoValueList());
-        // this property receives a default value, just to have the application running on dummy data
-        form.setFormaContato(new Long((long)-2113222835));
-        form.setFormaContatoValueList(new Object[] {"formaContato-1", "formaContato-2", "formaContato-3", "formaContato-4", "formaContato-5"});
-        form.setFormaContatoLabelList(form.getFormaContatoValueList());
-        // this property receives a default value, just to have the application running on dummy data
-        form.setTipoDemanda(new Long((long)1375887778));
-        form.setTipoDemandaValueList(new Object[] {"tipoDemanda-1", "tipoDemanda-2", "tipoDemanda-3", "tipoDemanda-4", "tipoDemanda-5"});
-        form.setTipoDemandaLabelList(form.getTipoDemandaValueList());
-    }
+  
+
+	@Override
+	public void recuperaCidades(ActionMapping mapping, RecuperaCidadesForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void recuperaEstados(ActionMapping mapping, RecuperaEstadosForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {		
+		HashMap mapa = new HashMap();
+    	Iterator it = getDemandanteService().recuperarEstados().iterator();
+    	while(it.hasNext()){
+    		Estado estado = (Estado) it.next();
+    		mapa.put(estado.getId(), estado.getUf());
+    	}
+        //form.setFormaContato(new Long((long)-2113222835));
+        form.setEstadoValueList(mapa.keySet().toArray());
+        form.setEstadoLabelList(mapa.values().toArray()); 
+	}
+
+	@Override
+	public void recuperaTiposDemandante(ActionMapping mapping, RecuperaTiposDemandanteForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
