b439236c624b35802681a851a6d396afcd72a506
phtcosta <phtcosta@b14c6362-024a-0410-a50b-db045366b063>
2008-04-21 23:33:28 +0000

    criando criteria para pesquisar demandas

--- trunk/web/src/java/com/googlecode/ouvidoria/apresentacao/demanda/pesquisa/PesquisaDemandaCTLImpl.java
+++ trunk/web/src/java/com/googlecode/ouvidoria/apresentacao/demanda/pesquisa/PesquisaDemandaCTLImpl.java
@@ -1,6 +1,7 @@
 // license-header java merge-point
 package com.googlecode.ouvidoria.apresentacao.demanda.pesquisa;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 
@@ -10,83 +11,78 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.struts.action.ActionMapping;
 
 import com.googlecode.ouvidoria.negocio.demanda.Assunto;
+import com.googlecode.ouvidoria.negocio.demanda.DemandaCriteria;
 import com.googlecode.ouvidoria.negocio.demanda.FormaContato;
 import com.googlecode.ouvidoria.negocio.demanda.TipoDemanda;
 
 /**
  * @see com.googlecode.ouvidoria.apresentacao.demanda.pesquisa.PesquisaDemandaCTL
  */
-public class PesquisaDemandaCTLImpl extends PesquisaDemandaCTL
-{  
+public class PesquisaDemandaCTLImpl extends PesquisaDemandaCTL {
 
-    /**
-     * This dummy variable is used to populate the "demandas" table.
-     * You may delete it when you add you own code in this controller.
-     */
-    private static final java.util.Collection demandasDummyList =
-        java.util.Arrays.asList( new Object[] {
-            new DemandasDummy("tipoDemanda-1", "assunto-1", "dataCriacao-1"),
-            new DemandasDummy("tipoDemanda-2", "assunto-2", "dataCriacao-2"),
-            new DemandasDummy("tipoDemanda-3", "assunto-3", "dataCriacao-3"),
-            new DemandasDummy("tipoDemanda-4", "assunto-4", "dataCriacao-4"),
-            new DemandasDummy("tipoDemanda-5", "assunto-5", "dataCriacao-5")
-        } );
+	/**
+	 * This dummy variable is used to populate the "demandas" table. You may
+	 * delete it when you add you own code in this controller.
+	 */
+	private static final java.util.Collection demandasDummyList = java.util.Arrays.asList(new Object[] { new DemandasDummy("tipoDemanda-1", "assunto-1", "dataCriacao-1"), new DemandasDummy("tipoDemanda-2", "assunto-2", "dataCriacao-2"),
+			new DemandasDummy("tipoDemanda-3", "assunto-3", "dataCriacao-3"), new DemandasDummy("tipoDemanda-4", "assunto-4", "dataCriacao-4"), new DemandasDummy("tipoDemanda-5", "assunto-5", "dataCriacao-5") });
 
-    /**
-     * This inner class is used in the dummy implementation in order to get the web application
-     * running without any manual programming.
-     * You may delete this class when you add you own code in this controller.
-     */
-    public static final class DemandasDummy implements java.io.Serializable
-    {
-        private String tipoDemanda = null;
-        private String assunto = null;
-        private String dataCriacao = null;
+	/**
+	 * This inner class is used in the dummy implementation in order to get the
+	 * web application running without any manual programming. You may delete
+	 * this class when you add you own code in this controller.
+	 */
+	public static final class DemandasDummy implements java.io.Serializable {
+		private String tipoDemanda = null;
+		private String assunto = null;
+		private String dataCriacao = null;
 
-        public DemandasDummy(String tipoDemanda, String assunto, String dataCriacao)
-        {
-            this.tipoDemanda = tipoDemanda;
-            this.assunto = assunto;
-            this.dataCriacao = dataCriacao;
-        }
-        
-        public void setTipoDemanda(String tipoDemanda)
-        {
-            this.tipoDemanda = tipoDemanda;
-        }
+		public DemandasDummy(String tipoDemanda, String assunto, String dataCriacao) {
+			this.tipoDemanda = tipoDemanda;
+			this.assunto = assunto;
+			this.dataCriacao = dataCriacao;
+		}
 
-        public String getTipoDemanda()
-        {
-            return this.tipoDemanda;
-        }
-        
-        public void setAssunto(String assunto)
-        {
-            this.assunto = assunto;
-        }
+		public void setTipoDemanda(String tipoDemanda) {
+			this.tipoDemanda = tipoDemanda;
+		}
 
-        public String getAssunto()
-        {
-            return this.assunto;
-        }
-        
-        public void setDataCriacao(String dataCriacao)
-        {
-            this.dataCriacao = dataCriacao;
-        }
+		public String getTipoDemanda() {
+			return this.tipoDemanda;
+		}
 
-        public String getDataCriacao()
-        {
-            return this.dataCriacao;
-        }
-        
-    }
+		public void setAssunto(String assunto) {
+			this.assunto = assunto;
+		}
+
+		public String getAssunto() {
+			return this.assunto;
+		}
+
+		public void setDataCriacao(String dataCriacao) {
+			this.dataCriacao = dataCriacao;
+		}
+
+		public String getDataCriacao() {
+			return this.dataCriacao;
+		}
+
+	}
 
 	@Override
 	public void recuperaDemandas(ActionMapping mapping, RecuperaDemandasForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
-		
-		System.out.println("recupera demandas ...");
+		System.out.println("recupera demandas ...........................................");
+
+		DemandaCriteria criteria = new DemandaCriteria();
+		criteria.setAssunto(form.getAssunto());
+		criteria.setDataFim(form.getDataFimAsDate());
+		criteria.setDataInicio(form.getDataInicioAsDate());
+		criteria.setFormaContato(form.getFormaContato());
+		criteria.setTipoDemanda(form.getTipoDemanda());
+
+		Collection demandas = getDemandaService().pesquisaDemandas(criteria);
+		if (demandas != null)
+			form.setDemandas(demandas);
 	}
 
 	@Override
@@ -95,37 +91,37 @@ public class PesquisaDemandaCTLImpl extends PesquisaDemandaCTL
 		recuperaFormasContato(form);
 		recuperaTiposDemanda(form);
 	}
-	
-	 private void recuperaFormasContato(PopulaTelaPesquisaForm form) throws Exception {
-			HashMap mapa = new HashMap();
-			Iterator it = getDemandaService().recuperaFormasContato().iterator();
-			while (it.hasNext()) {
-				FormaContato forma = (FormaContato) it.next();
-				mapa.put(forma.getId(), forma.getNome());
-			}
-			form.setFormaContatoValueList(mapa.keySet().toArray());
-			form.setFormaContatoLabelList(mapa.values().toArray());
-		}
-	    
-	    private void recuperaTiposDemanda(PopulaTelaPesquisaForm form) throws Exception {
-			HashMap mapa = new HashMap();
-			Iterator it = getDemandaService().recuperaTiposDemanda().iterator();
-			while (it.hasNext()) {
-				TipoDemanda tipo = (TipoDemanda) it.next();
-				mapa.put(tipo.getId(), tipo.getNome());
-			}
-			form.setTipoDemandaValueList(mapa.keySet().toArray());
-			form.setTipoDemandaLabelList(mapa.values().toArray());
-		}
 
-		private void recuperaAssuntos(PopulaTelaPesquisaForm form) throws Exception {
-			HashMap mapa = new HashMap();
-			Iterator it = getDemandaService().recuperaAssuntos().iterator();
-			while (it.hasNext()) {
-				Assunto assunto = (Assunto) it.next();
-				mapa.put(assunto.getId(), assunto.getNome());
-			}
-			form.setAssuntoValueList(mapa.keySet().toArray());
-			form.setAssuntoLabelList(mapa.values().toArray());
+	private void recuperaFormasContato(PopulaTelaPesquisaForm form) throws Exception {
+		HashMap mapa = new HashMap();
+		Iterator it = getDemandaService().recuperaFormasContato().iterator();
+		while (it.hasNext()) {
+			FormaContato forma = (FormaContato) it.next();
+			mapa.put(forma.getId(), forma.getNome());
 		}
+		form.setFormaContatoValueList(mapa.keySet().toArray());
+		form.setFormaContatoLabelList(mapa.values().toArray());
+	}
+
+	private void recuperaTiposDemanda(PopulaTelaPesquisaForm form) throws Exception {
+		HashMap mapa = new HashMap();
+		Iterator it = getDemandaService().recuperaTiposDemanda().iterator();
+		while (it.hasNext()) {
+			TipoDemanda tipo = (TipoDemanda) it.next();
+			mapa.put(tipo.getId(), tipo.getNome());
+		}
+		form.setTipoDemandaValueList(mapa.keySet().toArray());
+		form.setTipoDemandaLabelList(mapa.values().toArray());
+	}
+
+	private void recuperaAssuntos(PopulaTelaPesquisaForm form) throws Exception {
+		HashMap mapa = new HashMap();
+		Iterator it = getDemandaService().recuperaAssuntos().iterator();
+		while (it.hasNext()) {
+			Assunto assunto = (Assunto) it.next();
+			mapa.put(assunto.getId(), assunto.getNome());
+		}
+		form.setAssuntoValueList(mapa.keySet().toArray());
+		form.setAssuntoLabelList(mapa.values().toArray());
+	}
 }
