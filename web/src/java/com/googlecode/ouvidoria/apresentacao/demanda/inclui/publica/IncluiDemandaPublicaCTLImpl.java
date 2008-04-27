6590909893343e869802215f8b1b82605506ef91
phtcosta <phtcosta@b14c6362-024a-0410-a50b-db045366b063>
2008-04-27 18:52:31 +0000

    cadastrando demanda pública

--- web/src/java/com/googlecode/ouvidoria/apresentacao/demanda/inclui/publica/IncluiDemandaPublicaCTLImpl.java
+++ web/src/java/com/googlecode/ouvidoria/apresentacao/demanda/inclui/publica/IncluiDemandaPublicaCTLImpl.java
@@ -11,11 +11,13 @@ import org.apache.struts.action.ActionMapping;
 
 import com.googlecode.ouvidoria.negocio.demanda.Assunto;
 import com.googlecode.ouvidoria.negocio.demanda.Demanda;
+import com.googlecode.ouvidoria.negocio.demanda.FormaContato;
 import com.googlecode.ouvidoria.negocio.demanda.TipoDemanda;
 import com.googlecode.ouvidoria.negocio.demandante.Cidade;
 import com.googlecode.ouvidoria.negocio.demandante.Demandante;
 import com.googlecode.ouvidoria.negocio.demandante.Estado;
 import com.googlecode.ouvidoria.negocio.demandante.TipoDemandante;
+import com.googlecode.ouvidoria.negocio.usuario.Usuario;
 
 /**
  * @see com.googlecode.ouvidoria.apresentacao.demanda.inclui.publica.IncluiDemandaPublicaCTL
@@ -28,29 +30,29 @@ public class IncluiDemandaPublicaCTLImpl extends IncluiDemandaPublicaCTL {
 	 *      javax.servlet.http.HttpServletResponse)
 	 */
 	public final void preCadastraDemanda(ActionMapping mapping, com.googlecode.ouvidoria.apresentacao.demanda.inclui.publica.PreCadastraDemandaForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
-		//TODO como setar uma FormaContato padrao??
+		
 		Demanda demanda = Demanda.Factory.newInstance();
+		
+		//TODO gerar senha de acompanhamento
+		demanda.setSenhaAcompanhamento("123");
+		
+		//TODO como setar uma FormaContato padrao??
+		demanda.setFormaDeContato(FormaContato.Factory.newInstance());
+		demanda.getFormaDeContato().setId(1L);
+		
+		//TODO como setar um usuario padrao??
+		demanda.setUsuarioCriacao(Usuario.Factory.newInstance());
+		demanda.getUsuarioCriacao().setId(2L);		
+		
 		demanda.setAssunto(Assunto.Factory.newInstance());
 		demanda.getAssunto().setId(form.getAssunto());
+		
 		demanda.setTipoDemanda(TipoDemanda.Factory.newInstance());
 		demanda.getTipoDemanda().setId(form.getTipoDemanda());
+		
 		demanda.setMensagem(form.getMensagem());
 				
-		getGerenteSessaoDemanda(request).setDemanda(demanda);
-		
-		// this property receives a default value, just to have the application
-		// running on dummy data
-		form.setMensagem("mensagem-test");
-		// this property receives a default value, just to have the application
-		// running on dummy data
-		form.setAssunto(new Long((long) -704303915));
-		form.setAssuntoValueList(new Object[] { "assunto-1", "assunto-2", "assunto-3", "assunto-4", "assunto-5" });
-		form.setAssuntoLabelList(form.getAssuntoValueList());
-		// this property receives a default value, just to have the application
-		// running on dummy data
-		form.setTipoDemanda(new Long((long) 1375887778));
-		form.setTipoDemandaValueList(new Object[] { "tipoDemanda-1", "tipoDemanda-2", "tipoDemanda-3", "tipoDemanda-4", "tipoDemanda-5" });
-		form.setTipoDemandaLabelList(form.getTipoDemandaValueList());
+		getGerenteSessaoDemanda(request).setDemanda(demanda);		
 	}
 
 	/**
@@ -61,49 +63,23 @@ public class IncluiDemandaPublicaCTLImpl extends IncluiDemandaPublicaCTL {
 	 */
 	public final void preCadastraDemandante(ActionMapping mapping, com.googlecode.ouvidoria.apresentacao.demanda.inclui.publica.PreCadastraDemandanteForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
 		Demandante demandante = Demandante.Factory.newInstance();
+		
 		demandante.setCep(form.getCep());
-		demandante.setCidade(Cidade.Factory.newInstance());
-		demandante.getCidade().setId(form.getCidade());
 		demandante.setDocumento(form.getDocumento());//TODO formatar ... pesquisar antes de salvar (usar o q ja existe)
 		demandante.setEmail(form.getEmail());
 		demandante.setEndereco(form.getEndereco());
 		demandante.setNome(form.getNome());
 		demandante.setTelefone(form.getTelefone());
+		
+		demandante.setCidade(Cidade.Factory.newInstance());
+		demandante.getCidade().setId(form.getCidade());
+		System.out.println("cidade: "+form.getCidade());
+				
 		demandante.setTipoDemandante(TipoDemandante.Factory.newInstance());
 		demandante.getTipoDemandante().setId(form.getTipoDemandante());
+		System.out.println("tipo_demandante: "+form.getTipoDemandante());
 		
-		getGerenteSessaoDemanda(request).setDemandante(demandante);
-		
-		// this property receives a default value, just to have the application
-		// running on dummy data
-		form.setNome("nome-test");
-		// this property receives a default value, just to have the application
-		// running on dummy data
-		form.setDocumento("documento-test");
-		// this property receives a default value, just to have the application
-		// running on dummy data
-		form.setEmail("email-test");
-		// this property receives a default value, just to have the application
-		// running on dummy data
-		form.setTelefone("telefone-test");
-		// this property receives a default value, just to have the application
-		// running on dummy data
-		form.setEstado(new Long((long) -1293665946));
-		form.setEstadoValueList(new Object[] { "estado-1", "estado-2", "estado-3", "estado-4", "estado-5" });
-		form.setEstadoLabelList(form.getEstadoValueList());
-		// this property receives a default value, just to have the application
-		// running on dummy data
-		form.setCidade(new Long((long) -1360636124));
-		form.setCidadeValueList(new Object[] { "cidade-1", "cidade-2", "cidade-3", "cidade-4", "cidade-5" });
-		form.setCidadeLabelList(form.getCidadeValueList());
-		// this property receives a default value, just to have the application
-		// running on dummy data
-		form.setEndereco("endereco-test");
-		// this property receives a default value, just to have the application
-		// running on dummy data
-		form.setTipoDemandante(new Long((long) -2094969219));
-		form.setTipoDemandanteValueList(new Object[] { "tipoDemandante-1", "tipoDemandante-2", "tipoDemandante-3", "tipoDemandante-4", "tipoDemandante-5" });
-		form.setTipoDemandanteLabelList(form.getTipoDemandanteValueList());
+		getGerenteSessaoDemanda(request).setDemandante(demandante);		
 	}
 
 	/**
@@ -113,8 +89,6 @@ public class IncluiDemandaPublicaCTLImpl extends IncluiDemandaPublicaCTL {
 	 *      javax.servlet.http.HttpServletResponse)
 	 */
 	public final void cadastraDemanda(ActionMapping mapping, com.googlecode.ouvidoria.apresentacao.demanda.inclui.publica.CadastraDemandaForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// nothing to be done for this operation, there are no properties that
-		// can be set
 		Demanda demanda = getGerenteSessaoDemanda(request).getDemanda();
 		Demandante demandante = getGerenteSessaoDemanda(request).getDemandante();
 		//TODO criar  o demandante anonimo (padrao)
@@ -138,12 +112,8 @@ public class IncluiDemandaPublicaCTLImpl extends IncluiDemandaPublicaCTL {
 	 */
 	public final void populaTelaDemandante(ActionMapping mapping, com.googlecode.ouvidoria.apresentacao.demanda.inclui.publica.PopulaTelaDemandanteForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
 		recuperaEstados(form);
+		recuperaCidades(form);
 		recuperaTiposDemandantes(form);
-		//TODO this property receives a default value, just to have the application
-		// running on dummy data
-		form.setCidade(new Long((long) -1360636124));
-		form.setCidadeValueList(new Object[] { "cidade-1", "cidade-2", "cidade-3", "cidade-4", "cidade-5" });
-		form.setCidadeLabelList(form.getCidadeValueList());
 	}
 
 	/**
@@ -201,4 +171,19 @@ public class IncluiDemandaPublicaCTLImpl extends IncluiDemandaPublicaCTL {
 		form.setEstadoLabelList(mapa.values().toArray());
 	}
 
+	private void recuperaCidades(com.googlecode.ouvidoria.apresentacao.demanda.inclui.publica.PopulaTelaDemandanteForm form) throws Exception {
+		String uf = (String) form.getEstadoLabelList()[0];
+		recuperaCidades(form, uf);
+	}
+	
+	private void recuperaCidades(com.googlecode.ouvidoria.apresentacao.demanda.inclui.publica.PopulaTelaDemandanteForm form, String uf) throws Exception {		
+		HashMap mapa = new HashMap();
+		Iterator it = getDemandanteService().recuperarCidadesDoEstado(uf).iterator();
+		while (it.hasNext()) {
+			Cidade cidade = (Cidade) it.next();
+			mapa.put(cidade.getId(), cidade.getNome());
+		}
+		form.setCidadeValueList(mapa.keySet().toArray());
+		form.setCidadeLabelList(mapa.values().toArray());
+	}
 }
