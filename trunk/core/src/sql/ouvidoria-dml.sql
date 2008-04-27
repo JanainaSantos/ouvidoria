dc539e0ffe651fd0c6184fcae644c0e9f887d1d3
phtcosta <phtcosta@b14c6362-024a-0410-a50b-db045366b063>
2008-04-27 18:48:23 +0000

    script contendo dados para teste do sistema

--- trunk/core/src/sql/ouvidoria-dml.sql
+++ trunk/core/src/sql/ouvidoria-dml.sql
@@ -1,33 +1,47 @@
-insert into forma_contato(id,nome,descricao) values(1,'E-mail','Contato feito atraves de email');
-insert into forma_contato(id,nome,descricao) values(2,'Telefone','Contato feito atraves de telefone');
-insert into forma_contato(id,nome,descricao) values(3,'Carta','Contato feito atraves de carta');
-insert into forma_contato(id,nome,descricao) values(4,'Pessoalmente','Contato feito pessoalmente');
-insert into forma_contato(id,nome,descricao) values(5,'Site','Contato feito atraves do site da ouvidoria');
+-- Dados "dummy" para execucao do sistema a nivel de testes
 
-insert into tipo_demanda(id,nome,descricao) values(1,'Critica','...');
-insert into tipo_demanda(id,nome,descricao) values(2,'Denuncia','...');
-insert into tipo_demanda(id,nome,descricao) values(3,'Elogio','...');
-insert into tipo_demanda(id,nome,descricao) values(4,'Reclamacao','...');
-insert into tipo_demanda(id,nome,descricao) values(5,'Sugestao','...');
+insert into perfil(id,nome,descricao) values(nextval('PERFIL_SEQ'),'administrador','...');
+insert into perfil(id,nome,descricao) values(nextval('PERFIL_SEQ'),'convidado','...');
 
-insert into assunto(id,nome,descricao) values(1,'Agricultura Familiar','...');
-insert into assunto(id,nome,descricao) values(2,'Estoques Publicos','...');
-insert into assunto(id,nome,descricao) values(3,'Programas de Subvencao Economica','...');
-insert into assunto(id,nome,descricao) values(4,'Doacao de Alimentos','...');
-insert into assunto(id,nome,descricao) values(5,'Armazenagem','...');
-insert into assunto(id,nome,descricao) values(6,'Convenios e Transferencias','...');
-insert into assunto(id,nome,descricao) values(7,'PNUD','...');
-insert into assunto(id,nome,descricao) values(8,'Levantamento de Safras','...');
-insert into assunto(id,nome,descricao) values(9,'Recursos Financeiros/Orcamentarios/Contabil','...');
-insert into assunto(id,nome,descricao) values(10,'Recursos Humanos','...');
-insert into assunto(id,nome,descricao) values(11,'Recursos Materiais','...');
-insert into assunto(id,nome,descricao) values(12,'Patrimonio','...');
-insert into assunto(id,nome,descricao) values(13,'Tecnologia da Informacao','...');
-insert into assunto(id,nome,descricao) values(14,'Licitacao e Contratos','...');
-insert into assunto(id,nome,descricao) values(15,'Outros ...','...');
+-- pedro=123
+-- convidado=convidado
+insert into usuario(id,login,senha,nome,email,ramal,perfil_fk) values(nextval('USUARIO_SEQ'),'pedro','ICy5YqxZB1uWSwcVLSNLcA==','Pedro Costa','phtcosta@hotmail.com','6441',1);
+insert into usuario(id,login,senha,nome,email,ramal,perfil_fk) values(nextval('USUARIO_SEQ'),'convidado','RGwFXlkc6QfevueOX3Gx/Q==','Convidado','convidado@hotmail.com','-',2);
 
-insert into estado values(1,'Distrito Federal','DF');
-insert into cidade values(1,'Brasilia',1);
-insert into demandante values(1,'pedro','123','end','123','mail',1,1);
-insert into demanda values(1,'2008-04-21','msg','','','123',1,1,1,1,1);
-insert into demanda values(2,'2008-04-21','msg2','','','123',1,1,1,1,1);
+insert into forma_contato(id,nome,descricao) values(nextval('FORMA_CONTATO_SEQ'),'E-mail','Contato feito atraves de email');
+insert into forma_contato(id,nome,descricao) values(nextval('FORMA_CONTATO_SEQ'),'Telefone','Contato feito atraves de telefone');
+insert into forma_contato(id,nome,descricao) values(nextval('FORMA_CONTATO_SEQ'),'Carta','Contato feito atraves de carta');
+insert into forma_contato(id,nome,descricao) values(nextval('FORMA_CONTATO_SEQ'),'Pessoalmente','Contato feito pessoalmente');
+insert into forma_contato(id,nome,descricao) values(nextval('FORMA_CONTATO_SEQ'),'Site','Contato feito atraves do site da ouvidoria');
+
+insert into tipo_demanda(id,nome,descricao) values(nextval('TIPO_DEMANDA_SEQ'),'Critica','...');
+insert into tipo_demanda(id,nome,descricao) values(nextval('TIPO_DEMANDA_SEQ'),'Denuncia','...');
+insert into tipo_demanda(id,nome,descricao) values(nextval('TIPO_DEMANDA_SEQ'),'Elogio','...');
+insert into tipo_demanda(id,nome,descricao) values(nextval('TIPO_DEMANDA_SEQ'),'Reclamacao','...');
+insert into tipo_demanda(id,nome,descricao) values(nextval('TIPO_DEMANDA_SEQ'),'Sugestao','...');
+
+insert into tipo_demandante(id,nome,descricao) values(nextval('TIPO_DEMANDA_SEQ'),'Cidadao','...');
+insert into tipo_demandante(id,nome,descricao) values(nextval('TIPO_DEMANDA_SEQ'),'Empresa','...');
+insert into tipo_demandante(id,nome,descricao) values(nextval('TIPO_DEMANDA_SEQ'),'Empregado','...');
+insert into tipo_demandante(id,nome,descricao) values(nextval('TIPO_DEMANDA_SEQ'),'Terceirizado','...');
+insert into tipo_demandante(id,nome,descricao) values(nextval('TIPO_DEMANDA_SEQ'),'Estagiario','...');
+
+insert into assunto(id,nome,descricao) values(nextval('ASSUNTO_SEQ'),'Agricultura Familiar','...');
+insert into assunto(id,nome,descricao) values(nextval('ASSUNTO_SEQ'),'Estoques Publicos','...');
+insert into assunto(id,nome,descricao) values(nextval('ASSUNTO_SEQ'),'Programas de Subvencao Economica','...');
+insert into assunto(id,nome,descricao) values(nextval('ASSUNTO_SEQ'),'Doacao de Alimentos','...');
+insert into assunto(id,nome,descricao) values(nextval('ASSUNTO_SEQ'),'Armazenagem','...');
+insert into assunto(id,nome,descricao) values(nextval('ASSUNTO_SEQ'),'Convenios e Transferencias','...');
+insert into assunto(id,nome,descricao) values(nextval('ASSUNTO_SEQ'),'PNUD','...');
+insert into assunto(id,nome,descricao) values(nextval('ASSUNTO_SEQ'),'Levantamento de Safras','...');
+insert into assunto(id,nome,descricao) values(nextval('ASSUNTO_SEQ'),'Recursos Financeiros/Orcamentarios/Contabil','...');
+insert into assunto(id,nome,descricao) values(nextval('ASSUNTO_SEQ'),'Recursos Humanos','...');
+insert into assunto(id,nome,descricao) values(nextval('ASSUNTO_SEQ'),'Recursos Materiais','...');
+insert into assunto(id,nome,descricao) values(nextval('ASSUNTO_SEQ'),'Patrimonio','...');
+insert into assunto(id,nome,descricao) values(nextval('ASSUNTO_SEQ'),'Tecnologia da Informacao','...');
+insert into assunto(id,nome,descricao) values(nextval('ASSUNTO_SEQ'),'Licitacao e Contratos','...');
+insert into assunto(id,nome,descricao) values(nextval('ASSUNTO_SEQ'),'Outros ...','...');
+
+insert into estado values(nextval('ESTADO_SEQ'),'Distrito Federal','DF');
+insert into cidade values(nextval('CIDADE_SEQ'),'Brasilia',1);
+
