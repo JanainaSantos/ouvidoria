00fd1424089ca670b4b87f4b12341d63fbc41237
phtcosta <phtcosta@b14c6362-024a-0410-a50b-db045366b063>
2014-11-22 14:04:31 +0000

    scripts para popular tabelas

---
+++ core/src/main/sql/complaint.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_complaint_type(id, name, description, active) VALUES (nextval('tb_complaint_type_seq'), 'CRITICA', '', true);
+INSERT INTO tb_complaint_type(id, name, description, active) VALUES (nextval('tb_complaint_type_seq'), 'DENUNCIA', '', true);
+INSERT INTO tb_complaint_type(id, name, description, active) VALUES (nextval('tb_complaint_type_seq'), 'ELOGIO', '', true);
+INSERT INTO tb_complaint_type(id, name, description, active) VALUES (nextval('tb_complaint_type_seq'), 'INFORMACAO', '', true);
+INSERT INTO tb_complaint_type(id, name, description, active) VALUES (nextval('tb_complaint_type_seq'), 'RECLAMACAO', '', true);
+INSERT INTO tb_complaint_type(id, name, description, active) VALUES (nextval('tb_complaint_type_seq'), 'SOLICITACAO', '', true);
+INSERT INTO tb_complaint_type(id, name, description, active) VALUES (nextval('tb_complaint_type_seq'), 'SUGESTAO', '', true);
+INSERT INTO tb_complaint_type(id, name, description, active) VALUES (nextval('tb_complaint_type_seq'), 'OUTROS', '', true);
+
+--INSERT INTO tb_subject(name, description, active) VALUES (?, ?, ?);
